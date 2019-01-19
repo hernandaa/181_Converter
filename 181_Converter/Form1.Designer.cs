ae006da8860942265a893503308e8353223dd4b9
hernandaa <42990222+hernandaa@users.noreply.github.com>
2019-01-19 19:44:21 +0700

    Initial commit

---
+++ 181_Converter/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace _181_Converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.fileTextbox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.searchBtn = new Bunifu.Framework.UI.BunifuImageButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.beginProcessBtn = new Bunifu.Framework.UI.BunifuFlatButton();
+            this.pdfOfd = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.searchBtn)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // fileTextbox
+            // 
+            this.fileTextbox.BackColor = System.Drawing.Color.NavajoWhite;
+            this.fileTextbox.Font = new System.Drawing.Font("Calibri", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fileTextbox.ForeColor = System.Drawing.Color.SteelBlue;
+            this.fileTextbox.Location = new System.Drawing.Point(7, 41);
+            this.fileTextbox.Name = "fileTextbox";
+            this.fileTextbox.Size = new System.Drawing.Size(431, 32);
+            this.fileTextbox.TabIndex = 0;
+            this.fileTextbox.Click += new System.EventHandler(this.fileTextbox_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(3, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(384, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Konfirmasi Perubahan Arahan Investasi (FM)";
+            // 
+            // searchBtn
+            // 
+            this.searchBtn.BackColor = System.Drawing.Color.Transparent;
+            this.searchBtn.Image = ((System.Drawing.Image)(resources.GetObject("searchBtn.Image")));
+            this.searchBtn.ImageActive = null;
+            this.searchBtn.Location = new System.Drawing.Point(444, 41);
+            this.searchBtn.Name = "searchBtn";
+            this.searchBtn.Size = new System.Drawing.Size(38, 32);
+            this.searchBtn.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.searchBtn.TabIndex = 2;
+            this.searchBtn.TabStop = false;
+            this.searchBtn.Zoom = 10;
+            this.searchBtn.Click += new System.EventHandler(this.searchBtn_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(465, 3);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "X";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // beginProcessBtn
+            // 
+            this.beginProcessBtn.Activecolor = System.Drawing.Color.FromArgb(((int)(((byte)(103)))), ((int)(((byte)(103)))), ((int)(((byte)(199)))));
+            this.beginProcessBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(103)))), ((int)(((byte)(103)))), ((int)(((byte)(199)))));
+            this.beginProcessBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.beginProcessBtn.BorderRadius = 0;
+            this.beginProcessBtn.ButtonText = " Begin Process";
+            this.beginProcessBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.beginProcessBtn.DisabledColor = System.Drawing.Color.Gray;
+            this.beginProcessBtn.Iconcolor = System.Drawing.Color.Transparent;
+            this.beginProcessBtn.Iconimage = ((System.Drawing.Image)(resources.GetObject("beginProcessBtn.Iconimage")));
+            this.beginProcessBtn.Iconimage_right = null;
+            this.beginProcessBtn.Iconimage_right_Selected = null;
+            this.beginProcessBtn.Iconimage_Selected = null;
+            this.beginProcessBtn.IconMarginLeft = 0;
+            this.beginProcessBtn.IconMarginRight = 0;
+            this.beginProcessBtn.IconRightVisible = true;
+            this.beginProcessBtn.IconRightZoom = 0D;
+            this.beginProcessBtn.IconVisible = true;
+            this.beginProcessBtn.IconZoom = 70D;
+            this.beginProcessBtn.IsTab = false;
+            this.beginProcessBtn.Location = new System.Drawing.Point(352, 122);
+            this.beginProcessBtn.Name = "beginProcessBtn";
+            this.beginProcessBtn.Normalcolor = System.Drawing.Color.FromArgb(((int)(((byte)(103)))), ((int)(((byte)(103)))), ((int)(((byte)(199)))));
+            this.beginProcessBtn.OnHovercolor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(127)))), ((int)(((byte)(224)))));
+            this.beginProcessBtn.OnHoverTextColor = System.Drawing.Color.White;
+            this.beginProcessBtn.selected = false;
+            this.beginProcessBtn.Size = new System.Drawing.Size(130, 36);
+            this.beginProcessBtn.TabIndex = 4;
+            this.beginProcessBtn.Text = " Begin Process";
+            this.beginProcessBtn.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.beginProcessBtn.Textcolor = System.Drawing.Color.White;
+            this.beginProcessBtn.TextFont = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.beginProcessBtn.Click += new System.EventHandler(this.beginProcessBtn_Click);
+            // 
+            // pdfOfd
+            // 
+            this.pdfOfd.FileName = "PDF File";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(57)))), ((int)(((byte)(57)))), ((int)(((byte)(143)))));
+            this.ClientSize = new System.Drawing.Size(494, 170);
+            this.Controls.Add(this.beginProcessBtn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.searchBtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.fileTextbox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            ((System.ComponentModel.ISupportInitialize)(this.searchBtn)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox fileTextbox;
+        private System.Windows.Forms.Label label1;
+        private Bunifu.Framework.UI.BunifuImageButton searchBtn;
+        private System.Windows.Forms.Label label2;
+        private Bunifu.Framework.UI.BunifuFlatButton beginProcessBtn;
+        private System.Windows.Forms.OpenFileDialog pdfOfd;
+    }
+}
+
---
+++ 181_Converter/Form1.cs
@@ -0,0 +1,249 @@
+﻿using Aspose.Cells;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _181_Converter
+{
+    public partial class Form1 : Form
+    {
+        DataTable dtNasabah = new DataTable();
+
+        public Form1()
+        {
+            InitializeComponent();
+            initialDeclarement();
+        }
+
+        protected void initialDeclarement()
+        {
+            dtNasabah.Columns.Add("NAMA_CUSTOMER");
+            dtNasabah.Columns.Add("NO_CUSTOMER");
+            dtNasabah.Columns.Add("ALAMAT");
+            dtNasabah.Columns.Add("KODEPOS");
+            dtNasabah.Columns.Add("HALAMAN");
+        }
+
+        #region layoutManagement
+        int MouseX = 0, MouseY = 0;
+        bool mouseDown;
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseDown = true;
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouseDown = false;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mouseDown)
+            {
+                MouseX = MousePosition.X;
+                MouseY = MousePosition.Y;
+
+                this.SetDesktopLocation(MouseX, MouseY);
+            }
+        }
+        #endregion
+
+        private void searchBtn_Click(object sender, EventArgs e)
+        {
+            this.pdfOfd.ShowDialog();
+            this.fileTextbox.Text = this.pdfOfd.FileName;
+        }
+
+        private void fileTextbox_Click(object sender, EventArgs e)
+        {
+            this.pdfOfd.ShowDialog();
+            this.fileTextbox.Text = this.pdfOfd.FileName;
+        }
+
+        private void beginProcessBtn_Click(object sender, EventArgs e)
+        {
+            string filePath = this.fileTextbox.Text;
+            string fileName = Path.GetFileNameWithoutExtension(filePath);
+
+            PdfReader pdfReader = new PdfReader(filePath);
+            int pageSum = pdfReader.NumberOfPages;
+
+            for (int x = 1; x <= pdfReader.NumberOfPages; x++)
+            {
+                dtNasabah.Rows.Add();
+                dtNasabah.Rows[x - 1]["HALAMAN"] = x;
+
+                ITextExtractionStrategy ITES = new iTextSharp.text.pdf.parser.SimpleTextExtractionStrategy(); //simple read with accessible
+                string textExctractor = PdfTextExtractor.GetTextFromPage(pdfReader, x, ITES);
+                string[] rowData = textExctractor.Split('\n');
+
+                int flagRow = 0;
+                for (int j = 0; j < rowData.Length; j++)
+                {
+                    int noCust = 0;
+                    if (rowData[j].Length > 6)
+                        int.TryParse(rowData[j].Substring(0, 6), out noCust);
+
+                    if (noCust != 0)
+                    {
+                        dtNasabah.Rows[x - 1]["NO_CUSTOMER"] = rowData[j].ToString();
+                        dtNasabah.Rows[x - 1]["NAMA_CUSTOMER"] = rowData[j + 1].ToString();
+                    }
+                    else if (rowData[j].Contains("JL. "))
+                    {
+                        flagRow = j;
+                    }
+                    else if (rowData[j].Contains("Dengan hormat,"))
+                    {
+                        string mergedAddress = "";
+                        for (int o = flagRow; o <= j - 2; o++)
+                        {
+                            if (!rowData[o].StartsWith("{") && !rowData[o].StartsWith("-"))
+                                mergedAddress += (o < j - 2) ? rowData[o] + " " : rowData[o] + ".";
+                        }
+                        dtNasabah.Rows[x - 1]["ALAMAT"] = mergedAddress;
+                        
+
+                        string[] rowsdata = rowData[j - 1].ToString().Split(' ');
+                        dtNasabah.Rows[x - 1]["KODEPOS"] = rowsdata[rowsdata.Count() - 1];
+                    }
+                }
+            }
+
+            exportToXls(dtNasabah);
+            creatingPdf();
+        }
+
+        #region excel
+        private void exportToXls(DataTable dtNasabah)
+        {
+            Workbook workbook = new Workbook();
+            workbook.Worksheets.Clear();
+
+            Styles styles = workbook.Styles;
+            int styleIndex = styles.Add();
+            Style styleHeader = styles[styleIndex];
+            styleHeader.Font.Name = "Arial";
+            styleHeader.Font.Size = 10;
+            styleHeader.Font.IsBold = true;
+            styleHeader.Pattern = BackgroundType.Solid;
+            styleHeader.ForegroundColor = Color.FromArgb(0, 204, 255);
+            styleHeader.Borders[BorderType.LeftBorder].LineStyle = CellBorderType.Thin;
+            styleHeader.Borders[BorderType.TopBorder].LineStyle = CellBorderType.Thin;
+            styleHeader.Borders[BorderType.RightBorder].LineStyle = CellBorderType.Thin;
+            styleHeader.Borders[BorderType.BottomBorder].LineStyle = CellBorderType.Thin;
+
+            styleIndex = styles.Add();
+            Style styleData = styles[styleIndex];
+            styleData.Borders[BorderType.LeftBorder].LineStyle = CellBorderType.Thin;
+            styleData.Borders[BorderType.TopBorder].LineStyle = CellBorderType.Thin;
+            styleData.Borders[BorderType.RightBorder].LineStyle = CellBorderType.Thin;
+            styleData.Borders[BorderType.BottomBorder].LineStyle = CellBorderType.Thin;
+
+            styleIndex = styles.Add();
+            Style styleAngka = styles[styleIndex];
+            styleAngka.HorizontalAlignment = TextAlignmentType.Center;
+            styleAngka.Borders[BorderType.LeftBorder].LineStyle = CellBorderType.Thin;
+            styleAngka.Borders[BorderType.TopBorder].LineStyle = CellBorderType.Thin;
+            styleAngka.Borders[BorderType.RightBorder].LineStyle = CellBorderType.Thin;
+            styleAngka.Borders[BorderType.BottomBorder].LineStyle = CellBorderType.Thin;
+
+            styleIndex = styles.Add();
+            Style stylePemisah = styles[styleIndex];
+            stylePemisah.Borders[BorderType.LeftBorder].LineStyle = CellBorderType.Thin;
+            stylePemisah.Borders[BorderType.TopBorder].LineStyle = CellBorderType.Thin;
+            stylePemisah.Borders[BorderType.RightBorder].LineStyle = CellBorderType.Thin;
+            stylePemisah.Borders[BorderType.BottomBorder].LineStyle = CellBorderType.Thin;
+            stylePemisah.Pattern = BackgroundType.Solid;
+            stylePemisah.ForegroundColor = Color.Yellow;
+            stylePemisah.Font.IsBold = true;
+
+            Worksheet workSheet = workbook.Worksheets.Add("Hasil_Output");
+
+            for (int i = 0; i < dtNasabah.Columns.Count; i++)
+            {
+                workSheet.Cells[0, i].PutValue(dtNasabah.Columns[i].ColumnName);
+                workSheet.Cells[0, i].Style = styleHeader;
+            }
+            int k = 0;
+
+            for (int i = 0; i < dtNasabah.Rows.Count; i++)
+            {
+                for (int j = 0; j < dtNasabah.Columns.Count; j++)
+                {
+                    if (j == 3 || j == 4)
+                    {
+                        workSheet.Cells[(i + 1), j].PutValue(dtNasabah.Rows[i][j].ToString());
+                        workSheet.Cells[(i + 1), j].Style = styleAngka;
+                    }
+                    else
+                    {
+                        workSheet.Cells[(i + 1), j].PutValue(dtNasabah.Rows[i][j].ToString());
+                        workSheet.Cells[(i + 1), j].Style = styleData;
+                    }
+                }
+            }
+            for (int x = 0; x < dtNasabah.Columns.Count; x++)
+            {
+                workSheet.AutoFitColumn(x);
+            }
+
+            workbook.Save("output.xls");
+            MessageBox.Show("Excel file created successful.");
+        }
+        #endregion
+
+        #region pdf
+        protected void creatingPdf()
+        {
+            string outputPath = Directory.GetCurrentDirectory() + "\\output.pdf";
+            string pdfPath = this.fileTextbox.Text;
+            PdfReader reader = new PdfReader(pdfPath);
+
+            var document = new Document(PageSize.A4);
+            var stream = File.OpenWrite(outputPath);
+            var writer = PdfWriter.GetInstance(document, stream);
+            writer.PdfVersion = PdfWriter.VERSION_1_5;
+
+            document.Open();
+            PdfContentByte pb = writer.DirectContent;
+
+            for(int a = 1; a <= reader.NumberOfPages; a++)
+            {
+                document.NewPage();
+                PdfImportedPage page1 = writer.GetImportedPage(reader, a);
+                pb.AddTemplate(page1, 0, 0);
+
+                BarcodeQRCode qrcodeAtas = new BarcodeQRCode(DateTime.Now.ToString("yyyyMMdd") + a.ToString("D6"), 70, 70, null);
+                iTextSharp.text.Image image1 = qrcodeAtas.GetImage();
+                image1.ScaleAbsolute(35, 35);
+                iTextSharp.text.Image mask1 = qrcodeAtas.GetImage();
+                mask1.MakeMask();
+                image1.ImageMask = mask1;
+                image1.SetAbsolutePosition(109, 730);
+                pb.AddImage(image1);
+            }
+            document.Close();
+
+            MessageBox.Show("PDF file created successful.");
+        }
+        #endregion
+    }
+}
